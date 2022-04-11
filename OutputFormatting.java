c5d1fc836dea619d6105a4540e2ca29104867ffd
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-11 18:09:38 +0300

    Update OutputFormatting.java

--- OutputFormatting.java
+++ OutputFormatting.java
@@ -10,6 +10,10 @@ public class Solution {
         for(int i = 0; i < 3; i++) {
             String sc = s.next();
             int x = s.nextInt();
+            
+            /* ("%-15s%03d%n", sc, x) % : used as a formatter. '-' : minus sign used for left 
+            indentation of the string. 15s : here 15 denotes the string's minimum field width 15. 
+            '0' : pads the extra 0s in the integer. 3d : here 3 denotes integer's minimum field width 3. %n : prints the new line */
             System.out.printf("%-15s%03d%n", sc, x);
         }
         
